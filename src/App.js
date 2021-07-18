45f6a35e7eea117050f45eee4b61e93cefba28df
sahrohit <sahrohit9586@hmail.com>
2021-07-19 00:27:39 +0545

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useEffect, useState } from "react";
+
+import Socials from "./components/Socials";
+import "./App.css";
 
 function App() {
+  const [isHovering, setIsHovering] = useState(false);
+
+  useEffect(() => {
+    const interval = setInterval(
+      () => {
+        const show = document.querySelector("span[data-show]");
+        const next =
+          show.nextElementSibling || document.querySelector("span:first-child");
+        const up = document.querySelector("span[data-up]");
+
+        if (!isHovering) {
+          if (up) {
+            up.removeAttribute("data-up");
+          }
+
+          show.removeAttribute("data-show");
+          show.setAttribute("data-up", "");
+
+          next.setAttribute("data-show", "");
+        }
+      },
+      isHovering ? 0 : 2000
+    );
+
+    return () => clearInterval(interval);
+  }, [isHovering]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <React.Fragment>
+      <h2>
+        This Domain is reserved
+        <div
+          className="mask"
+          onMouseEnter={() => {
+            setIsHovering(true);
+            console.log("Hovering");
+          }}
+          onMouseLeave={() => {
+            setIsHovering(false);
+            console.log("Not Hovering");
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <span data-show>by Rohit Kumar Sah</span>
+          <span>+977 9818506752</span>
+          <span>for portfolio.</span>
+        </div>
+      </h2>
+      <Socials />
+    </React.Fragment>
   );
 }
 
---
+++ src/components/Socials.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+import GitHubIcon from "@material-ui/icons/GitHub";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import TwitterIcon from "@material-ui/icons/Twitter";
+
+import "./Socials.css";
+import { Button } from "@material-ui/core";
+
+const Socials = () => {
+  return (
+    <React.Fragment>
+      <Button
+        onClick={(event) =>
+          (window.location.href = "https://github.com/sahrohit")
+        }
+      >
+        <GitHubIcon className="github_icon" />
+      </Button>
+      <Button
+        onClick={(event) =>
+          (window.location.href =
+            "https://www.facebook.com/imadeyoureadthis26/")
+        }
+      >
+        <FacebookIcon className="facebook_icon" />
+      </Button>
+      <Button
+        onClick={(event) =>
+          (window.location.href =
+            "https://www.linkedin.com/in/rohit-sah-96a10a1b8/")
+        }
+      >
+        <LinkedInIcon className="linkedin_icon" />
+      </Button>
+      <Button
+        onClick={(event) =>
+          (window.location.href = "https://twitter.com/sah_rohite")
+        }
+      >
+        <TwitterIcon className="twitter_icon" />
+      </Button>
+    </React.Fragment>
+  );
+};
+
+export default Socials;
